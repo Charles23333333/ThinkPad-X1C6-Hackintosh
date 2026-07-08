dd2c0f749d4f8411064ef286e66ab0473dd08302
Charles23333333 <118661526+Charles23333333@users.noreply.github.com>
2026-07-08 09:54:58 +0800

    Delete voltageshift/setup_tcc.sh

--- voltageshift/setup_tcc.sh
+++
@@ -1,7 +0,0 @@
-while true
-do
-  /Users/admin/Documents/voltageshift/voltageshift write 0x1A2 0x01640000 &&
-  /Users/admin/Documents/voltageshift/voltageshift wrmem 0xfed159a0 0x00ffffff &&
-  /Users/admin/Documents/voltageshift/voltageshift wrmem 0xfed159a4 0x00ffffff
-  sleep 5
-done
